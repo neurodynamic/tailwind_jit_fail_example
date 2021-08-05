9506691f02c937404e1675a20cc0772dcff32a71
neurodynamic <>
2021-08-05 10:34:08 -0700

    removes extra code/packages

--- js/app.ts
+++ js/app.ts
@@ -2,23 +2,3 @@
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
 import "../css/app.sass"
-import Alpine from "alpinejs"
-
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import deps with the dep name or local files with a relative path, for example:
-//
-//     import {Socket} from "phoenix"
-//     import socket from "./socket"
-//
-import topbar from "topbar"
-
-let csrfToken = document.querySelector("meta[name='csrf-token']")?.getAttribute("content")
-
-
-// Show progress bar on live navigation and form submits
-topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
